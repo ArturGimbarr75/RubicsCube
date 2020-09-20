cb78c753211211e9312797efce7c7c9f6c99bee0
Artur <Artur.Radzivilov@stud.vgtu.lt>
2020-09-20 18:20:06 +0300

    Removed useless code

--- Assets/Scripts/Managers/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -24,36 +24,20 @@ public class CameraManager : MonoBehaviour
     {
         transform.RotateAround
         (
-            new Vector3(),
+            Vector3.zero,
             Vector3.up,
             Settings.CameraSettings.HorizontalRotationSpeed * CameraJoystick.Horizontal * Time.deltaTime
         );
 
         var pos = transform.position;
-        var XZprojection = Mathf.Sqrt(Mathf.Pow(Settings.CameraSettings.CameraDistanceFromCenter, 2) - Mathf.Pow(pos.y, 2));
-        var hypotenuse = Mathf.Sqrt(Mathf.Pow(pos.x, 2) + Mathf.Pow(pos.z, 2));
-        var aspectRatio = Mathf.Round(hypotenuse / XZprojection);
-        if (Math.Abs(aspectRatio) > 0.98f)
-            aspectRatio = 1.0f;
-        pos = new Vector3
-        (
-            pos.x * aspectRatio,
-            Mathf.Clamp
+        pos.y = Mathf.Clamp
             (
                 pos.y + CameraJoystick.Vertical * Time.deltaTime * Settings.CameraSettings.VerticalRotationSpeed,
                 Settings.CameraSettings.VerticalMin,
                 Settings.CameraSettings.VerticalMax
-            ),
-            pos.z * aspectRatio
-        );
+            );
         transform.position = pos;
 
         transform.LookAt(new Vector3());
-    }
-
-    private decimal RoundUp(decimal number, int digits)
-    {
-        var factor = Convert.ToDecimal(Math.Pow(10, digits));
-        return Math.Ceiling(number * factor) / factor;
     }
 }
